e7430afc5a70ed10ba815591f3e0f0240ebc0f2e
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-05-12 21:25:16 -0400

    PROY 14 -183. Mostrando la información en el Modal

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -41,7 +41,9 @@ const Receta = ({ receta }) => {
 
   // Extraer Valores del Context
 
-  const { guardarIdReceta } = useContext(ModalContext);
+  const { informacion, guardarIdReceta, guardarReceta } = useContext(
+    ModalContext
+  );
 
   return (
     <div className="col-md-4 mb-3">
@@ -67,11 +69,15 @@ const Receta = ({ receta }) => {
             open={open}
             onClose={() => {
               guardarIdReceta(null);
+              guardarReceta({});
               handleClose();
             }}
           >
             <div style={modalStyle} className={classes.paper}>
-              <h1>Desde Modal</h1>
+              <h2>{informacion.strDrink}</h2>
+              <h3 className="mt-4">Instrucciones</h3>
+              <p>{informacion.strInstructions}</p>
+              <img className="img-fluid my-4" src={informacion.strDrinkThumb} />
             </div>
           </Modal>
         </div>
